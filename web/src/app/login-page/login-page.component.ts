dc08b98da52928ccca8d732a5f9f2906c0d922bb
braistud <brainstorm352@gmail.com>
2019-02-17 21:56:44 +0300

    fix action url

--- web/src/app/login-page/login-page.component.ts
+++ web/src/app/login-page/login-page.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {AuthService} from '../shared/services/auth.service'
+import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
-  form: FormGroup;
-
-  constructor(private auth: AuthService) {
+  form: FormGroup
+  aSub:Subscription
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
 
   }
 
@@ -20,12 +24,30 @@ export class LoginPageComponent implements OnInit {
       nickname: new FormControl(null,[Validators.required,Validators.minLength(5)]),
       password: new FormControl(null,[Validators.required, Validators.minLength(6)])
     })
+
+    this.route.queryParams.subscribe((params:Params) => {
+      if (params['registered']) {
+        //можете зайти со своими данными
+      } else if (params['accessDenied']){
+        //сначала авторизуйтесь 
+      }
+    })
   }
+
+  ngOnDestroy() {
+    if (this.aSub) {
+    this.aSub.unsubscribe()
+    }
+  }
+
   onSubmit(){
+    this.form.disable()
+
     this.auth.login(this.form.value).subscribe(
-      () => console.log('Login success'),
+      () => console.log('Login success')/*this.router.navigate['url']*/,
       error => {
         console.warn(error);
+        this.form.enable()
       }
     )
   }
