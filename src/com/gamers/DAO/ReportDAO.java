e1f56de455f33d7f23f6c92987eb0a65ef32e779
allacee <ddimaglushkov@ya.ru>
2019-02-17 16:01:44 +0300

    actions added

---
+++ src/com/gamers/DAO/ReportDAO.java
@@ -0,0 +1,10 @@
+package com.gamers.DAO;
+
+import com.gamers.Entities.Report;
+
+public class ReportDAO extends DAOService<Report, Long>{
+
+    //TODO: add some jms logic
+    public ReportDAO(){super(Report.class);}
+
+}
